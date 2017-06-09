37d0fe2aa678d112f5543c13453bad85d666aaea
Luiz Gustavo <luiz.gustavo358@gmail.com>
2017-06-09 16:11:15 -0300

    altas parada: tela de configurações minhas tarefas tela do projeto

--- app/src/main/java/com/luizgbraganca/avante123/Configuracoes.java
+++ app/src/main/java/com/luizgbraganca/avante123/Configuracoes.java
@@ -2,12 +2,45 @@ package com.luizgbraganca.avante123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Configuracoes extends AppCompatActivity {
+public class Configuracoes extends AppCompatActivity
+{
+    private ImageView batman, deadpool, homemAranha, homemDeFerro;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        batman = (ImageView) findViewById(R.id.batman);
+        deadpool = (ImageView) findViewById(R.id.deadpool);
+        homemAranha = (ImageView) findViewById(R.id.homemAranha);
+        homemDeFerro = (ImageView) findViewById(R.id.homemDeFerro);
+    }
+
+    public void batman(View v)
+    {
+        Toast.makeText(Configuracoes.this, "Batman nanananana :)", Toast.LENGTH_SHORT).show();
+
+        batman.setImageResource(R.drawable.batman2);
+    }
+
+    public void deadpool(View view)
+    {
+        Toast.makeText(Configuracoes.this, "Deadpool", Toast.LENGTH_SHORT).show();
+    }
+
+    public void homemAranha(View view)
+    {
+        Toast.makeText(Configuracoes.this, "Homem Aranha", Toast.LENGTH_SHORT).show();
+    }
+
+    public void homemDeFerro(View view)
+    {
+        Toast.makeText(Configuracoes.this, "Homem de Ferro", Toast.LENGTH_SHORT).show();
     }
 }
